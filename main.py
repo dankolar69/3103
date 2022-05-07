4e8969af28138f175289e520464f59985ec78b9b
dankolar69 <91247767+dankolar69@users.noreply.github.com>
2022-05-07 20:25:00 +0200

    Aktualizovat pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,34 +1,58 @@
 radio.set_group(33)
+radio.set_transmit_power(7)
+radio.set_transmit_serial_number(True)
+control.device_serial_number()
 start = False
 
-def answer(odpoved):
-    global start
-    radio.send_value("answer", odpoved)
+#pokud je zahájeno hlasování a stisknete např. tlačítko A microbit ukáže string "A", pro potvrzení musíte zmáčknout tlačítko ještě jednou
+#pokud přijme number 3, server zaznamenal hlas
+#stisknutím tlačítka A hlasuji pro A
+#stisknutím tlačítka B hlasuji pro B
+#stisknutím tlačítka AB hlasuji pro C
 
-    if input.button_is_pressed(Button.A):
-        answer(65)
-        RadioPacketProperty.SERIAL_NUMBER
-        start = False
-
-    if input.button_is_pressed(Button.B):
-        answer(66)
-        RadioPacketProperty.SERIAL_NUMBER
-        start = False
-
-    if input.pin_is_pressed(TouchPin.P0):
-        answer(67)
-        RadioPacketProperty.SERIAL_NUMBER
-        start = False
-    
 
 def on_received_number(receivedNumber):
     if receivedNumber == 1:
-        basic.show_string("S")
+        basic.show_string("S",1500)
         start = True
 
     if receivedNumber == 2:
-        basic.show_string("X")
+        basic.show_string("X",1500)
         start = False
+
+    if receivedNumber == 3:
+        basic.show_icon(IconNames.YES,1500)
 radio.on_received_number(on_received_number)
+
+
+
+
+def on_button_pressed_a():
+    if start == True:
+        basic.show_string("A")
+        if input.button_is_pressed(Button.A):
+            radio.send_value("answer", 1)
+            start = False
+            
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    if start == True:
+        basic.show_string("B")
+        if input.button_is_pressed(Button.B):
+            radio.send_value("answer", 2)
+            start = False
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def on_button_pressed_ab():
+    if start == True:
+        basic.show_string("C")
+        if input.button_is_pressed(Button.AB):
+            radio.send_value("answer", 1)
+            start = False
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+
+
 
 
--- main.ts
+++ main.ts
@@ -1,38 +1,63 @@
 radio.setGroup(33)
+radio.setTransmitPower(7)
+radio.setTransmitSerialNumber(true)
+control.deviceSerialNumber()
 let start = false
-function answer(odpoved: number) {
-    
-    radio.sendValue("answer", odpoved)
-    if (input.buttonIsPressed(Button.A)) {
-        answer(65)
-        RadioPacketProperty.SerialNumber
-        start = false
-    }
-    
-    if (input.buttonIsPressed(Button.B)) {
-        answer(66)
-        RadioPacketProperty.SerialNumber
-        start = false
-    }
-    
-    if (input.pinIsPressed(TouchPin.P0)) {
-        answer(67)
-        RadioPacketProperty.SerialNumber
-        start = false
-    }
-    
-}
-
+// pokud je zahájeno hlasování a stisknete např. tlačítko A microbit ukáže string "A", pro potvrzení musíte zmáčknout tlačítko ještě jednou
+// pokud přijme number 3, server zaznamenal hlas
+// stisknutím tlačítka A hlasuji pro A
+// stisknutím tlačítka B hlasuji pro B
+// stisknutím tlačítka AB hlasuji pro C
 radio.onReceivedNumber(function on_received_number(receivedNumber: number) {
     let start: boolean;
     if (receivedNumber == 1) {
-        basic.showString("S")
+        basic.showString("S", 1500)
         start = true
     }
     
     if (receivedNumber == 2) {
-        basic.showString("X")
+        basic.showString("X", 1500)
         start = false
+    }
+    
+    if (receivedNumber == 3) {
+        basic.showIcon(IconNames.Yes, 1500)
+    }
+    
+})
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    let start: boolean;
+    if (start == true) {
+        basic.showString("A")
+        if (input.buttonIsPressed(Button.A)) {
+            radio.sendValue("answer", 1)
+            start = false
+        }
+        
+    }
+    
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    let start: boolean;
+    if (start == true) {
+        basic.showString("B")
+        if (input.buttonIsPressed(Button.B)) {
+            radio.sendValue("answer", 2)
+            start = false
+        }
+        
+    }
+    
+})
+input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+    let start: boolean;
+    if (start == true) {
+        basic.showString("C")
+        if (input.buttonIsPressed(Button.AB)) {
+            radio.sendValue("answer", 1)
+            start = false
+        }
+        
     }
     
 })
